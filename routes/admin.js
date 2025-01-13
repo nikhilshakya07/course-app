d9468812545c499cfcc7aff2cf9f8c0a9b0e82cd
nikhilshakya07 <nikhilshakya1308@gmail.com>
2025-01-13 20:22:32 +0530

    save changes

--- routes/admin.js
+++ routes/admin.js
@@ -14,19 +14,19 @@ adminRouter.post("/signin", function(req, res){
     })
 })
 
-adminRouter.post("/course", function(req, res){
+adminRouter.post("/", function(req, res){
     res.json({
         message : " Admin course creating endpoint"
     })
 })
 
-adminRouter.put("/course", function(req, res){
+adminRouter.put("/", function(req, res){
     res.json({
         message : " Admin course editing endpoint"
     })
 })
 
-adminRouter.get("/course/bulk", function(req, res){
+adminRouter.get("/bulk", function(req, res){
     res.json({
         message : " Admin course content endpoint"
     })
